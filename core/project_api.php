b1e49923ccd43e0d890fcb9186d45f08577ab522
Carlos Proensa <proensa@gmail.com>
2019-03-13 00:36:06 +0100

    Clean up queries for project_get_all_user_rows()

--- core/project_api.php
+++ core/project_api.php
@@ -620,7 +620,6 @@ function project_get_all_user_rows( $p_project_id = ALL_PROJECTS, $p_access_leve
 		return array();
 	}
 
-	$t_on = ON;
 	$t_users = array();
 
 	$t_global_access_level = $p_access_level;
@@ -663,52 +662,40 @@ function project_get_all_user_rows( $p_project_id = ALL_PROJECTS, $p_access_leve
 		}
 	}
 
-	if( is_array( $t_global_access_level ) ) {
-		if( 0 == count( $t_global_access_level ) ) {
-			$t_global_access_clause = '>= ' . NOBODY . ' ';
-		} else if( 1 == count( $t_global_access_level ) ) {
-			$t_global_access_clause = '= ' . array_shift( $t_global_access_level ) . ' ';
-		} else {
-			$t_global_access_clause = 'IN (' . implode( ',', $t_global_access_level ) . ')';
-		}
-	} else {
-		$t_global_access_clause = '>= ' . $t_global_access_level . ' ';
-	}
-
 	if( $p_include_global_users ) {
-		db_param_push();
-		$t_query = 'SELECT id, username, realname, access_level
-				FROM {user}
-				WHERE access_level ' . $t_global_access_clause;
-
-		if ( !$p_include_disabled_users ) {
-			$t_query .= ' AND enabled = ' . db_param();
-			$t_result = db_query( $t_query, Array( $t_on ) );
+		$t_query = new DbQuery();
+		if( is_array( $t_global_access_level ) ) {
+			if( empty( $t_global_access_level ) ) {
+				$t_where = 'access_level >= ' . $t_query->param( NOBODY );
+			} else {
+				$t_where = $t_query->sql_in( 'access_level', $t_global_access_level );
+			}
 		} else {
-			$t_result = db_query( $t_query );
+			$t_where = 'access_level >= ' . $t_query->param( (int)$t_global_access_level );
 		}
+		if ( !$p_include_disabled_users ) {
+			$t_where .= ' AND enabled = ' . $t_query->param( true );
+		}
+		$t_sql = 'SELECT id, username, realname, access_level FROM {user} WHERE ' . $t_where;
+		$t_query->sql( $t_sql );
 
-		while( $t_row = db_fetch_array( $t_result ) ) {
+		while( $t_row = $t_query->fetch() ) {
 			$t_users[(int)$t_row['id']] = $t_row;
 		}
 	}
 
 	if( $c_project_id != ALL_PROJECTS ) {
 		# Get the project overrides
-		db_param_push();
-		$t_query = 'SELECT u.id, u.username, u.realname, l.access_level
-				FROM {project_user_list} l, {user} u
-				WHERE l.user_id = u.id
-				AND l.project_id = ' . db_param();
-
+		$t_sql = 'SELECT u.id, u.username, u.realname, l.access_level FROM {project_user_list} l'
+				. ' JOIN {user} u ON l.user_id = u.id'
+				. ' WHERE l.project_id = :project';
 		if ( !$p_include_disabled_users ) {
-			$t_query .= ' AND u.enabled = ' . db_param();
-			$t_result = db_query( $t_query, Array( $c_project_id, $t_on ) );
-		} else {
-			$t_result = db_query( $t_query, Array( $c_project_id ) );
+			$t_sql .= ' AND u.enabled = :enabled';
 		}
+		$t_params = array( 'project' => $c_project_id, 'enabled' => true );
+		$t_query = new DbQuery( $t_sql, $t_params );
 
-		while( $t_row = db_fetch_array( $t_result ) ) {
+		while( $t_row = $t_query->fetch() ) {
 			if( is_array( $p_access_level ) ) {
 				$t_keep = in_array( $t_row['access_level'], $p_access_level );
 			} else {
